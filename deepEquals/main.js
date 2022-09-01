566f0d136d24603074951e095ba75bd463adf847
nargiz1 <nargizer@code.edu.az>
2022-09-01 07:32:35 +0400

    changes

--- deepEquals/main.js
+++ deepEquals/main.js
@@ -21,7 +21,7 @@ function deepEquals(a, b) {
               typeof b[i] === "object" &&
               !(Array.isArray(a[i]) || Array.isArray(b[i]))
             ) {
-              if (object(a[i], b[i]) === false) {
+              if (deepEquals(a[i], b[i]) === false) {
                 return false;
               }
             } else if (primitives(a[i], b[i]) !== true) {
@@ -58,7 +58,7 @@ function deepEquals(a, b) {
               return false;
             }
           } else if (Array.isArray(a[key]) && Array.isArray(b[key])) {
-            if (arrays(a[key], b[key]) === false) {
+            if (deepEquals(a[key], b[key]) === false) {
               return false;
             }
           } else {
@@ -87,72 +87,6 @@ function primitives(a, b) {
     return true;
   }
   return false;
-}
-
-function arrays(a, b) {
-  if (Array.isArray(a) && Array.isArray(b)) {
-    if (a.length === b.length) {
-      if (a.length === 0 && b.length === 0) {
-        return true;
-      } else {
-        for (let i = 0; i < a.length; i++) {
-          if (Array.isArray(a[i]) && Array.isArray(b[i])) {
-            if (deepEquals(a[i], b[i]) === false) {
-              return false;
-            }
-          } else if (
-            a[i] !== null &&
-            b[i] !== null &&
-            typeof a[i] === "object" &&
-            typeof b[i] === "object" &&
-            !(Array.isArray(a[i]) || Array.isArray(b[i]))
-          ) {
-            if (object(a[i], b[i]) === false) {
-              return false;
-            }
-          } else if (primitives(a[i], b[i]) !== true) {
-            return false;
-          }
-        }
-      }
-      return true;
-    }
-  }
-  return false;
-}
-
-function object(a, b) {
-  if (Object.keys(a).length === Object.keys(b).length) {
-    if (Object.keys(a).length === 0 && Object.keys(b).length === 0) {
-      return true;
-    }
-    for (let i = 0; i < Object.keys(a).length; i++) {
-      if (Object.keys(b).some((x) => x === Object.keys(a)[i]) === false) {
-        return false;
-      }
-      let key = Object.keys(b).find((x) => x === Object.keys(a)[i]);
-      if (
-        a[key] !== null &&
-        b[key] !== null &&
-        typeof a[key] === "object" &&
-        typeof b[key] === "object" &&
-        !(Array.isArray(a[key]) || Array.isArray(b[key]))
-      ) {
-        if (object(a[key], b[key]) === false) {
-          return false;
-        }
-      } else if (Array.isArray(a[key]) && Array.isArray(b[key])) {
-        if (object(a[key], b[key]) === false) {
-          return false;
-        }
-      } else {
-        if (primitives(a[key], b[key]) !== true) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
 }
 
 // console.log("my  ", deepEquals(undefined, undefined));
@@ -230,9 +164,9 @@ function object(a, b) {
 // console.log(
 //   deepEquals({ a: 123, b: { c: [4, 5, 6] } }, { a: 123, b: { c: [4, 5, 6] } })
 // ); //true 
-// console.log(deepEquals({a: 123, b: {c: [4, 5, 6]}}), {b: {c: [4, 5, 6]}}) //false
-// console.log(deepEquals({a: 123, b: {c: [4, 5, 6]}}, {a: 123, b: {c: [4, "5", 6]}})) //false
-// console.log(deepEquals([1, 2, [3, 4]], [1, 2, [3, 4]])); //true
-// console.log(
-//   deepEquals([1, 2, [3, 4, { a: "abc" }]], [1, 2, [3, 4, { a: "abc" }]])
-// ); //true
+console.log(deepEquals({a: 123, b: {c: [4, 5, 6]}}), {b: {c: [4, 5, 6]}}) //false
+console.log(deepEquals({a: 123, b: {c: [4, 5, 6]}}, {a: 123, b: {c: [4, "5", 6]}})) //false
+console.log(deepEquals([1, 2, [3, 4]], [1, 2, [3, 4]])); //true
+console.log(
+  deepEquals([1, 2, [3, 4, { a: "abc" }]], [1, 2, [3, 4, { a: "abc" }]])
+); //true
